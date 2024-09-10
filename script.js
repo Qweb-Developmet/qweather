bf326c7f0b0ce9dea9500819e161e4a73f6e6f21
Hqurban <168327283+Hqurban@users.noreply.github.com>
2024-09-09 21:14:41 -0400

    created a script js code

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let search = document.getElementById(`search`);
-console.log(search);
-search.innerHTML(search.value);
+let form = document.querySelector("form");
+let searchinput = document.getElementById(`search`);
+let searchvalue = searchinput.value;
+function searchfn(event) {
+  event.preventDefault();
+
+  console.log(searchinput.value);
+  console.log(form);
+  //   console.log(searchvalue);
+  let city = document.querySelector(`.cityname`);
+  city.innerHTML = `${searchinput.value}`;
+  //   console.log(city);
+}
+form.addEventListener("submit", searchfn);

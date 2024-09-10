507ca2341c937169aeeae3af779e203a15c472dd
Hqurban <168327283+Hqurban@users.noreply.github.com>
2024-09-09 23:33:19 -0400

    added some details to html

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-let form = document.querySelector("form");
-let searchinput = document.getElementById(`search`);
-let searchvalue = searchinput.value;
-function searchfn(event) {
+function searchhandlingfn(event) {
   event.preventDefault();
-
-  console.log(searchinput.value);
-  console.log(form);
-  //   console.log(searchvalue);
-  let city = document.querySelector(`.cityname`);
-  city.innerHTML = `${searchinput.value}`;
-  //   console.log(city);
 }
-form.addEventListener("submit", searchfn);
+let form = document.querySelector("form");
+form.addEventListener("submit", searchhandlingfn);
+// searchCity(searchinput.kabul);
